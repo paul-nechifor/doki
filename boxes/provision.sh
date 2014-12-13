427267464123b0350cc86777d1384cb3b06a4829
Paul Nechifor <paul@nechifor.net>
2014-12-13 15:39:50 +0000

    Switch to and configure MySQL.

--- boxes/provision.sh
+++ boxes/provision.sh
@@ -2,24 +2,34 @@
 
 packages=(
   httpd
-  mariadb
+  mysql
+  mysql-devel
+  mysql-server
+  python-devel
   python-pip
 )
 
 main() {
   install_packages
   install_pip_packages
+  configure_mysql
   setup_requirements
 }
 
 install_packages() {
-  #yum -y update
+  yum -y update
+  yum -y groupinstall "Development Tools"
   rpm -ivh http://dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
   yum -y install ${packages[@]}
 }
 
 install_pip_packages() {
   pip install virtualenv
+}
+
+configure_mysql() {
+  chkconfig mysqld on
+  service mysqld start
 }
 
 setup_requirements() {
