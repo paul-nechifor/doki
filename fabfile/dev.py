c5e50bba281cf3cf6faabd64079c4a3f954a7e0c
Paul Nechifor <paul@nechifor.net>
2014-12-14 14:54:00 +0000

    Add syncdb to Fabric.

--- fabfile/dev.py
+++ fabfile/dev.py
@@ -5,14 +5,28 @@ dev_settings = {
     'user': 'vagrant',
 }
 
+def in_site(f):
+    def g():
+        with settings(**dev_settings):
+            with cd('/vagrant/src'):
+                with prefix('. ../env/bin/activate'):
+                    f()
+    g.__name__, g.__doc__ = f.__name__, f.__doc__
+    return g
+
 @task
 @roles('dev')
+@in_site
 def runserver():
-    """Connect to the dev box and start the server."""
-    with settings(**dev_settings):
-        with cd('/vagrant/src'):
-            with prefix('. ../env/bin/activate'):
-                run('./manage.py runserver 0.0.0.0:8000')
+    """Start the server on the dev box."""
+    run('./manage.py runserver 0.0.0.0:8000')
+
+@task
+@roles('dev')
+@in_site
+def syncdb():
+    """Sync the DB on the dev box."""
+    run('./manage.py syncdb')
 
 @task(default=True)
 def start():
